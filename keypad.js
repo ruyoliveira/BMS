87fb9904775c23b65db7561dfeabaca9140bf3eb
ruyoliveira <ruygsgoliveira@live.com>
2013-04-01 17:36:29 +0200

    Working asynchronously, validate user password obtained from db

--- keypad.js
+++ keypad.js
@@ -1,27 +1,41 @@
 // Keypad.js
-var User = require("./BMS/server/bms/User.js");
+var user = require("./BMS/server/bms/User.js");
+var adm_unlocker= new user();
 var password_buffer='';
-var port = process.argv[2];
-var serialport = require('serialport');
+adm_unlocker.load_user("adm",function(err,user_found){
+			if(err||!user_found) console.log("ERROR: Not possible to loggin as 'adm'");
+			adm_unlocker = user_found[0];
+			console.log("Logged as '"+adm_unlocker.name+"'\nEnter your password:\n");
+			main_keypad();
+								
+			}
+	);
 
-var sp = new serialport.SerialPort(port, {
-                baudrate: 9600,
-                parser: serialport.parsers.readline('\r\n')});
+function main_keypad(){
+	var port = process.argv[2];
+	var serialport = require('serialport');
 
-sp.on('data', function(line) {
-    
-    console.log(line[0]);
-    password_buffer=password_buffer+line[0];
-    
-    if(password.length>4){
-    	if(password=="11111"){
-    		console.log("Password CORRECT!");
-    		
-    		}
-    	else{
-    		console.log("Password Incorrect");
-    	}
-    	password='';
-    }
-    
-});
+	var sp = new serialport.SerialPort(port, {
+		        baudrate: 9600,
+		        parser: serialport.parsers.readline('\r\n')
+		        });
+
+	sp.on('data', function(line) {
+	
+	    	
+		console.log(line[0]);
+	    	password_buffer=password_buffer+line[0];
+	    
+	    	if(password_buffer.length>4){
+	    		if(password_buffer==adm_unlocker.password){
+	    			console.log("Password CORRECT!");
+	    		
+	    		}
+	    		else{
+	    		console.log("Password Incorrect");
+	    	}
+	    	password_buffer='';
+	    }
+	    
+	});
+}
