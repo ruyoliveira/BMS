1c41c81d25367014267f96fdb2b75dc559aea037
ruyoliveira <ruygsgoliveira@live.com>
2013-04-01 11:25:32 +0200

    Added reference to User.js

--- keypad.js
+++ keypad.js
@@ -1,5 +1,6 @@
 // Keypad.js
-var password='';
+var User = require("./BMS/server/bms/User.js");
+var password_buffer='';
 var port = process.argv[2];
 var serialport = require('serialport');
 
@@ -10,7 +11,7 @@ var sp = new serialport.SerialPort(port, {
 sp.on('data', function(line) {
     
     console.log(line[0]);
-    password=password+line[0];
+    password_buffer=password_buffer+line[0];
     
     if(password.length>4){
     	if(password=="11111"){
